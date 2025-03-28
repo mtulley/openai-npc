f2fc06ea1f64d5fee4c1afb0144b5500423e4e26
mtulley <mftulley@gmail.com>
2025-03-27 19:49:08 -0700

    Initial Commit
    Created player node, cow node, and game node. Structured project files.

---
+++ Player.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+using System.Xml.Serialization;
+
+public partial class Player : CharacterBody2D
+{
+	public int Speed = 200;
+
+	private AnimatedSprite2D animatedSprite2D;
+
+    Node areaParent;
+
+    public void GetInput() 
+    {
+        var inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+	    this.Velocity = inputDirection.Normalized() * Speed;
+    }
+
+	public override void _Ready()
+	{
+        // NOTE: Motion Mode is set to MOTION_MODE_FLOATING in the inspector
+
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        animatedSprite2D.Play("idle_down");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndSlide();
+    }
+
+    private void OnInteractionAreaEntered(Area2D area)
+    {
+        areaParent = area.GetParent();
+
+        if (areaParent.IsInGroup("NPCs"))
+        {
+            var nameLabel = areaParent.GetNode<Label>("NameLabel");
+            nameLabel.Visible = true;
+        }
+    }
+
+    private void OnInteractionAreaExited(Area2D area)
+    {
+        areaParent = area.GetParent();
+
+        if (area.GetParent().IsInGroup("NPCs"))
+        {
+            area.GetParent().GetNode<Label>("NameLabel").Visible = false;
+        }
+    }
+}
+
